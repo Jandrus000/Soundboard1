dc3e9a01702ae36e5deb21099447efd7769248c9
Jandrus000 <jcandrus02@gmail.com>
2023-10-16 15:00:25 -0600

    creation

---
+++ script.js
@@ -0,0 +1,19 @@
+function playWeekAgo(){
+        const audio = new Audio("About A Week Ago -Sound Effect - Vine WSHH-.mp3");
+        audio.play();
+}
+function playPizzaHere(){
+
+        const audio = new Audio("Ayo the pizza here (Original meme).mp3");
+        audio.play();
+}
+
+function playVineBoom(){
+    const audio = new Audio("Vine boom sound effect.mp3");
+    audio.play();
+}
+
+function playRaar(){
+    const audio = new Audio("raar  arrr  yawn TikTok sound effect.mp3")
+    audio.play();
+}
