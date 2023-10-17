b998f4363053a5dd97e46f078bbec265d9d1f9a8
Jandrus000 <jcandrus02@gmail.com>
2023-10-17 10:10:30 -0600

    merged all functions to play audio

--- script.js
+++ script.js
@@ -1,22 +1,19 @@
-function playWeekAgo(){
+function playAudio(audioPick){
+    if(audioPick=='weekAgo'){
         const audio = new Audio("About A Week Ago -Sound Effect - Vine WSHH-.mp3");
         audio.play();
-}
-function playPizzaHere(){
-
+    }
+    if(audioPick=='pizzaHere'){
         const audio = new Audio("Ayo the pizza here (Original meme).mp3");
         audio.play();
-}
-
-function playVineBoom(){
-    const audio = new Audio("Vine boom sound effect.mp3");
-    audio.play();
-}
-
-function playRaar(word){
-    const audio = new Audio("raar  arrr  yawn TikTok sound effect.mp3")
-    audio.play();
-    if(word == 'string'){
-        console.log('word');
     }
+    if(audioPick=='vineBoom'){
+        const audio = new Audio("Vine boom sound effect.mp3");
+        audio.play();
+    }
+    if(audioPick=='raar'){
+        const audio = new Audio("raar  arrr  yawn TikTok sound effect.mp3")
+        audio.play();
+    }
+    
 }
